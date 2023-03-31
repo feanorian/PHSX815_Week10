3bb217a96ee7bb93e966edd4bcf1fcaff5000d26
Craig L Brooks <feanorian@users.noreply.github.com>
2023-03-31 10:48:16 -0500

    Update optimize2d.py

--- optimize2d.py
+++ optimize2d.py
@@ -11,11 +11,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.optimize import minimize
 
-# Define the 3D function to plot
-def f(xy):
-    return (2*xy[0])**2 + xy[1]**2 + 2
 
-# Create a meshgrid of x and y values
+def f(xy):
+    return 2*(xy[0])**2 + xy[1]**2 + 2
+
 x = np.linspace(-10, 10, 20)
 y = np.linspace(-10, 10, 20)
 X, Y = np.meshgrid(x, y)
@@ -23,24 +22,19 @@ X, Y = np.meshgrid(x, y)
 # Evaluate the function at each point in the meshgrid
 Z = f([X, Y])
 
-# Plot the function as a 3D surface
-fig = plt.figure()
-ax = fig.add_subplots(projection='3d')
-ax.plot_surface(X, Y, Z, cmap='viridis')
-
-# Find the minimum of the function using the minimize function from SciPy
+# Finds the minimum of the function 
 x0 = [0, 0] # Initial guess for the minimum
 optimize = minimize(f, x0)
 print(f"Minimum found at x = {optimize.x[0]}, y = {optimize.x[1]}, z = {f(optimize.x)}" )
 
-# Plot the minimum as a red dot
-ax.scatter(optimize.x[0], optimize.x[1], f(optimize.x), c='red', s=50)
+# Plot the function as a 3D surface and the minimum
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(X, Y, Z, cmap='cool')
 
-# Set the axis labels and title
+ax.scatter(optimize.x[0], optimize.x[1], f(optimize.x), c='red', s=50)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('f(x,y)')
 ax.set_title(f"Minimum found at x = {optimize.x[0]}, y = {optimize.x[1]}, z = {f(optimize.x)}")
-
-# Show the plot
 plt.show()
